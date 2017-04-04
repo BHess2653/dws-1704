05c21d7f531656ddbd26e670fa732f1814d8a752
BHess2653 <GM_Dahaka@BHMac.local>
2017-04-04 10:27:52 -0400

    updated index.js

--- js/index.js
+++ js/index.js
@@ -1,27 +1,7 @@
-class Main extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  
-  componentDidMount() {
-    const URL = 'http://pokeapi.co/api/v2/pokemon/150';
-    fetch(URL)
-      .then((data) => data.json())
-      .then((json) => {
-        this.setState(json);
-    });
-      
-  }
-  render() {
-    return (
-      <div>
-         <h3>Pokemon: {this.state.name}</h3>
-         <p>ID: {this.state.id}</p>
-         <p>Height: {this.state.height}, Weight: {this.state.weight}</p>
-      </div>
-    );
-  }
-}
+angular.module('pokeApp', []);
 
-ReactDOM.render(<Main />, document.getElementById('app'));
+angular.module('pokeApp').controller('pokeController', function($scope, pokeService) {
+
+  $scope.getPokemon();
+
+});
