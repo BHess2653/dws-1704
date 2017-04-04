4832bef5c89e0e7547b06ea433109fc76d16efbc
BHess2653 <GM_Dahaka@BHMac.local>
2017-04-04 10:35:19 -0400

    updated index.js pokeservice

--- js/index.js
+++ js/index.js
@@ -4,4 +4,26 @@ angular.module('pokeApp').controller('pokeController', function($scope, pokeServ
 
   $scope.getPokemon();
 
+  $scope.getMore = function(url) {
+    pokeService.getMore(url).then(function(results) {
+      $scope.pokeInfo = results;
+      console.dir($scope.pokeInfo);
+    });
+  };
+
+});
+
+angular.module('pokeApp').service('pokeService', function($http) {
+
+  var pokeUrl = 'http://pokeapi.co/api/v2/';
+
+  this.getMore = function(url) {
+    return $http({
+      method: 'GET',
+      url: url
+    }).then(function(results) {
+      return results.data;
+    });
+  };
+
 });
