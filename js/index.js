db29cc32dd7f1db25a4cd4d80e9b8c0471d4ec56
BHess2653 <GM_Dahaka@BHMac.local>
2017-04-27 11:03:29 -0400

    updated tooltip

--- js/index.js
+++ js/index.js
@@ -19,14 +19,62 @@ angular.module('pokeApp').controller('pokeController', function($scope, pokeServ
    $scope.getTip = function(name){
        console.log(name);
        switch(name){
-           case "poison":
-               return "don't eat this"
+           case "normal":
+               return "weak against: fighting"
                break;
-           case "etc":
-               return "boop"
+           case "fire":
+               return "weak against: ground, rock, water"
+               break;
+            case "water":
+               return "weak against: electric, grass"
+               break;
+            case "grass":
+               return "weak against: bug, fire, flying, ice, poison"
+               break;
+            case "electric":
+               return "weak against: ground"
+               break;
+            case "ice":
+               return "weak against: fighting, fire, rock, steel"
+               break;
+            case "fighting":
+               return "weak against: fairy, flying, psychic"
+               break;
+            case "poison":
+               return "weak against: ground, psychic"
+               break;
+            case "ground":
+               return "weak against: grass, ice, water"
+               break;
+            case "flying":
+               return "weak against: electric, ice, rock"
+               break;
+            case "psychic":
+               return "weak against: bug, dark, ghost"
+               break;
+            case "bug":
+               return "weak against: fire, flying, rock"
+               break;
+            case "rock":
+               return "weak against: fighting, grass, ground, steel, water"
+               break;
+            case "ghost":
+               return "weak against: dark, ghost"
+               break;
+            case "dark":
+               return "weak against: bug, fairy, fighting"
+               break;
+            case "dragon":
+               return "weak against: dragon, fairy, ice"
+               break;
+            case "steel":
+               return "weak against: fighting, fire, ground"
+               break;
+            case "fairy":
+               return "weak against: poison, steel"
                break;
            default:
-               return "fire"
+               return "boop"
                break;
        }
    }; /* ToolTip End */
